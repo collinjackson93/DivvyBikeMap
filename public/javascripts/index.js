ab31e7dcc45889da58c5244826051c0457b48f10
collinjackson93 <collinjackson93@gmail.com>
2015-07-22 20:32:36 -0500

    Directions service functional

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,6 +4,8 @@ var infoWindows = [];
 var openWindow;
 var startLocation;
 var endLocation;
+var directionsService;
+var directionsDisplay1, directionsDisplay2, directionsDisplay3;
 var stationsVisible = true;
 
 function initialize() {
@@ -63,8 +65,10 @@ function initialize() {
   });
 
   var end_input = (document.getElementById('end-input'));
+  var get_directions  = (document.getElementById('get-directions'));
   map.controls[google.maps.ControlPosition.LEFT_TOP].push(start_input);
   map.controls[google.maps.ControlPosition.LEFT_TOP].push(end_input);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(get_directions);
   map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(document.getElementById('panel'));
   var autocomplete_start = new google.maps.places.Autocomplete(start_input);
   autocomplete_start.bindTo('bounds', map);
@@ -73,12 +77,44 @@ function initialize() {
 
   google.maps.event.addListener(autocomplete_start, 'place_changed', function() {
     startLocation.setPosition(autocomplete_start.getPlace().geometry.location);
-    startLocation.setIcon('http://www.google.com/mapfiles/markerA.png');
+    startLocation.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|FE6256|000000');
   });
   google.maps.event.addListener(autocomplete_end, 'place_changed', function() {
     endLocation.setPosition(autocomplete_end.getPlace().geometry.location);
-    endLocation.setIcon('http://www.google.com/mapfiles/markerB.png');
+    endLocation.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=B|FE6256|000000');
     endLocation.setMap(map);
+  });
+
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay1 = new google.maps.DirectionsRenderer({
+    map: map,
+    suppressMarkers: true,
+    preserveViewport: true,
+    polylineOptions: {
+      strokeOpacity: 0,
+      icons: [{
+        icon: lineSymbol,
+        offset: '0',
+        repeat: '20px'
+      }]}
+  });
+  directionsDisplay2 = new google.maps.DirectionsRenderer({
+    map: map,
+    suppressMarkers: true,
+    preserveViewport: true,
+    suppressBicyclingLayer: true
+  });
+  directionsDisplay3 = new google.maps.DirectionsRenderer({
+    map: map,
+    suppressMarkers: true,
+    preserveViewport: true,
+    polylineOptions: {
+      strokeOpacity: 0,
+      icons: [{
+        icon: lineSymbol,
+        offset: '0',
+        repeat: '20px'
+      }]}
   });
 }
 
@@ -100,6 +136,12 @@ var geo_options = {
   maximumAge        : Infinity
 };
 
+var lineSymbol = {
+  path: 'M 0,-1 0,1',
+  strokeOpacity: 1,
+  scale: 4
+};
+
 function setAllMap(map) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
@@ -115,12 +157,18 @@ function toggleStations() {
   stationsVisible = !stationsVisible;
 }
 
-function findNearestStation(position) {
+function findNearestStation(position, start) {
   var closestStation;
   var shortestDistance;
+  var limitingFactor;
   for (var i = 0; i < data.stationBeanList.length; ++i) {
     var curStation = data.stationBeanList[i];
-    if (curStation.availableBikes > 0) {
+    if (start) {
+      limitingFactor = curStation.availableBikes;
+    } else {
+      limitingFactor = curStation.availableDocks;
+    }
+    if (limitingFactor > 0) {
       var dist = google.maps.geometry.spherical.computeDistanceBetween(position, markers[i].getPosition());
       if (closestStation == null || dist < shortestDistance) {
         closestStation = i;
@@ -128,11 +176,68 @@ function findNearestStation(position) {
       }
     }
   }
-  if(openWindow) {
-    openWindow.close()
-  }
-  infoWindows[closestStation].open(map, markers[closestStation]);
-  openWindow = infoWindows[closestStation];
+  return markers[closestStation].getPosition();
+  // if(openWindow) {
+  //   openWindow.close()
+  // }
+  // infoWindows[closestStation].open(map, markers[closestStation]);
+  // openWindow = infoWindows[closestStation];
+}
+
+function getDirections() {
+  var startPosition = startLocation.getPosition();
+  console.log(startPosition);
+  var endPosition = endLocation.getPosition();
+  console.log(endPosition);
+  var startStation = findNearestStation(startPosition, true);
+  console.log(startStation);
+  var endStation = findNearestStation(endPosition, false);
+  console.log(endStation);
+  startLocation.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|FE6256|000000');
+  endLocation.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=4|FE6256|000000');
+  var startStationMarker = new google.maps.Marker({
+    position: startStation,
+    map: map,
+    icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|FE6256|000000'
+  });
+  var endStationMarker = new google.maps.Marker({
+    position: endStation,
+    map: map,
+    icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=3|FE6256|000000'
+  });
+
+  var request1 = {
+    origin: startPosition,
+    destination: startStation,
+    travelMode: google.maps.TravelMode.WALKING
+  };
+  directionsService.route(request1, function(result, status) {
+    if(status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay1.setDirections(result);
+    }
+  });
+
+  var request2 = {
+    origin: startStation,
+    destination: endStation,
+    travelMode: google.maps.TravelMode.BICYCLING
+  };
+  directionsService.route(request2, function(result, status) {
+    if(status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay2.setDirections(result);
+    }
+  });
+
+  var request3 = {
+    origin: endStation,
+    destination: endPosition,
+    travelMode: google.maps.TravelMode.WALKING
+  };
+  directionsService.route(request3, function(result, status) {
+    if(status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay3.setDirections(result);
+    }
+  });
 }
 
 
